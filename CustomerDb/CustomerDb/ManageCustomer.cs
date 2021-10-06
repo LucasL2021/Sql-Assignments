fbba65c53c960399c16eb6dde44ccd583f76ceff
LucasL2021 <90743521+LucasL2021@users.noreply.github.com>
2021-10-05 23:17:20 -0400

    Add files via upload

---
+++ CustomerDb/CustomerDb/ManageCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerDb.Data.Repository;
+using CustomerDb.Data.Model;
+
+namespace CustomerDb
+{
+    class ManageCustomer
+    {
+        IRepository<Customer> customerRepository;
+        public ManageCustomer() 
+        {
+            customerRepository = new CustomerRepository();
+        }
+        void AddCustomer() 
+        {
+            Customer c = new Customer();
+            Console.Write("Enter CustomerId = ");
+            c.CustomerId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter FirstName = ");
+            c.FirstName = Console.ReadLine();
+            Console.Write("Enter LastName = ");
+            c.LastName = Console.ReadLine();
+            Console.Write("Enter Mobile = ");
+            c.Mobile = Console.ReadLine();
+            Console.Write("Enter EmailId = ");
+            c.EmailId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter City = ");
+            c.City = Console.ReadLine();
+            Console.Write("Enter State = ");
+            c.State = Console.ReadLine();
+        }
+        public void Run() 
+        {
+            AddCustomer();
+        }
+    }
+}
