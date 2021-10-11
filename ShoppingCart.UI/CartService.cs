dcf7b9819a498bd731006eec126754af1d98ce94
LucasL2021 <90743521+LucasL2021@users.noreply.github.com>
2021-10-11 10:30:26 -0400

    Add files via upload

---
+++ ShoppingCart.UI/CartService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingCart.Data.Models;
+
+namespace ShoppingCart.UI
+{
+    public class CartService
+    {
+        Dictionary<string, int> summary = new Dictionary<string, int>();
+        decimal totalPrice;
+        public void calTotalPrice(CartItems ci) 
+        {
+            foreach (var item in ci.cartList) 
+            {
+                if (item is Apple) 
+                {
+                    if (summary.TryGetValue("Apple", out int qty)) summary["Apple"] = summary["Apple"]+1;
+                    else summary.Add("Apple", 1);
+                }
+                if (item is Orange)
+                {
+                    if (summary.TryGetValue("Orange", out int qty)) summary["Orange"] = summary["Orange"] + 1;
+                    else summary.Add("Orange", 1);
+                }
+            }
+            Apple apple = new Apple();
+            Orange orange = new Orange();
+            totalPrice = summary["Apple"] / apple.PackQuantity * apple.PackPrice + summary["Apple"] % apple.PackQuantity * apple.UnitPrice + summary["Orange"] / orange.PackQuantity * orange.PackPrice + summary["Orange"] % orange.PackQuantity * orange.UnitPrice;
+            Console.WriteLine("The total price is " + totalPrice + " dollar.");
+
+        }
+    }
+}
