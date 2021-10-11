ae2fe8ad72a09269fac7d3a26514ee321e678194
LucasL2021 <90743521+LucasL2021@users.noreply.github.com>
2021-10-11 10:29:17 -0400

    Add files via upload

---
+++ ShoppingCart/ShoppingCart/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingCart.UI;
+using ShoppingCart.Data.Models;
+
+namespace ShoppingCart.App
+{
+    public class Menu
+    {
+        public void showMenu() 
+        {
+            Console.WriteLine("Please enter scan to begin scanning products; Enter exit to stop scanning:");
+            string str = Console.ReadLine();
+            Scan s = new Scan();
+            CartItems ci = new CartItems();
+            CartService cs = new CartService();
+
+            while (true)
+            {
+                Console.WriteLine("Please scan the product: ");
+                str = Console.ReadLine();
+                if (str == "exit") break;
+                s.scan(ci, str);
+                Console.WriteLine("Product is scanned.");
+            }
+            Console.WriteLine("Scanning is done.");
+            cs.calTotalPrice(ci);
+        }
+    }
+}
